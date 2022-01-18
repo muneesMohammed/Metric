97f19d0d9a089a2785dc1c55c5a6b09daca231c0
unknown <marhaba.munees@gmail.com>
2022-01-17 18:29:27 -0800

    initial commit

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div>
+            <div className="image">
+            <img src="https://png.pngtree.com/background/20210711/original/pngtree-gourmet-breakfast-snacks-simple-fresh-food-banner-picture-image_1087836.jpg" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Banner
